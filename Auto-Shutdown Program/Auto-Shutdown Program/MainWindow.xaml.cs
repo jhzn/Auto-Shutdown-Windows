1dbff5123d2f2cff6341f5b0d895344616e8ba0e
jhzn <jhakanzon@gmail.com>
2016-04-26 15:27:31 +0200

    Added files via upload

--- Auto-Shutdown Program/Auto-Shutdown Program/MainWindow.xaml.cs
+++ Auto-Shutdown Program/Auto-Shutdown Program/MainWindow.xaml.cs
@@ -24,20 +24,31 @@ namespace Auto_Shutdown_Program
         {
             InitializeComponent();
         }
+        public static int current = 0;
+
         private void button_Click_60min(object sender, RoutedEventArgs e)
         {
+            current = 60;
+            currentLabel1.Content = current;
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();
+
             startInfo.FileName = "cmd.exe";
             startInfo.Arguments = "/C shutdown -t 3600 -f -s";
             process.StartInfo = startInfo;
             process.Start();
         }
 
-
         private void button_Click_Avbryt(object sender, RoutedEventArgs e)
         {
+            //current = 0;
+            currentLabel1.Content = "";
+            currentLabel1.Content = current;
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
@@ -49,9 +60,17 @@ namespace Auto_Shutdown_Program
 
         private void button_Click_90min(object sender, RoutedEventArgs e)
         {
+            current = 90;
+            currentLabel1.Content = current;
+
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();
+
             startInfo.FileName = "cmd.exe";
             startInfo.Arguments = "/C shutdown -t 5400 -f -s";
             process.StartInfo = startInfo;
@@ -64,9 +83,53 @@ namespace Auto_Shutdown_Program
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();
+
+            startInfo.FileName = "cmd.exe";
             startInfo.Arguments = "/C shutdown -t 0 -f -s";
             process.StartInfo = startInfo;
             process.Start();
+        }
+
+        private void buttonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            current += 10;
+            currentLabel1.Content = current;
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();           
+            
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -t " + current * 60 + " -f -s";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void buttonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            current = current <= 10 ? current = 10 : current -= 10;
+            currentLabel1.Content = current;
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();
+
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -t " + current * 60 + " -f -s";
+            process.StartInfo = startInfo;
+            process.Start();
+
         }
     }
 }
